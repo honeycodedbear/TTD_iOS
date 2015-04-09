26b107275be815507f99b8fb808dff73ec992155
Max Rogers <maxrogers90@gmail.com>
2015-04-09 10:37:47 -0400

    Initial Commit

---
+++ TTD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTD
+//
+//  Created by Max Rogers on 4/9/15.
+//  Copyright (c) 2015 Max Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
